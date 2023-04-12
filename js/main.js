ce0d9606e10a73204db02077f0b4c0264a835292
David Cabello <davidivan.eda@gmail.com>
2023-04-11 23:32:02 -0300

    jquery api
    api

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.getElementById("calcular-btn").addEventListener("click", function () {
+    let presupuesto = document.getElementById("presupuesto-input").value;
+
+    $.ajax({
+        url: "https://api.bluelytics.com.ar/v2/latest",
+        method: "GET",
+        success: function (data) {
+            let blueDolar = data.blue.value_buy;
+            let totalArs = presupuesto;
+            let totalUsd = totalArs / blueDolar;
+            document.getElementById("presupuesto").innerHTML = "El costo total del producto en pesos argentinos es: " + totalArs + "<br>El costo total del producto en dólares es: " + totalUsd;
+        }
+    });
+});
